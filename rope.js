4e0aec5605434baa18b1a8df532310d914105be1
Sohamambre07 <82446205+Sohamambre07@users.noreply.github.com>
2021-06-07 21:13:59 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,30 @@
+class Rope{
+
+constructor(x,y,z)
+{
+    var options={
+    bodyA:x,
+    bodyB:y,
+    pointB:{x:z,y:0},
+    lenghth:255
+
+    }
+
+    this.rope= Constraint.create(options)
+    World.add(world,this.rope)
+
+this.pointB=z
+
+
+}
+
+display(){
+
+push ()
+strokeWeight(2)
+line(this.rope.bodyA.position.x,this.rope.bodyA.position.y,this.rope.bodyB.position.x +this.pointB, this.rope.bodyB.position.y)
+pop ()
+
+}
+
+}
